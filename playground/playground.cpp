422b337d615f3e83a126f1a15e08447304f562cf
Patrick Naughton <patricknaughton01@gmail.com>
2019-08-01 22:00:22 -0400

    Moving fp camera

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -11,6 +11,10 @@ GLFWwindow* window;
 using namespace glm;
 
 #include <common/shader.hpp>
+#include <common/texture.hpp>
+#include <common/controls.hpp>
+
+#define PI 3.141592653589793238
 
 int main( void )
 {
@@ -30,7 +34,7 @@ int main( void )
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open a window and create its OpenGL context
-    GLFWwindow* window;
+    // GLFWwindow* window;
     window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
     if (window == NULL){
         fprintf(stderr, "Failed to open GLFW window. If you have an Intel"
@@ -48,6 +52,8 @@ int main( void )
     glEnable(GL_DEPTH_TEST);
     // Accept fragment if closer to the camera
     glDepthFunc(GL_LESS);
+    // Cull unseen triangles
+    glEnable(GL_CULL_FACE);
 
     // Create a Vertex Array Object and set as current one
     GLuint VertexArrayID;
@@ -237,17 +243,9 @@ int main( void )
     // Get a handle on our "MVP" uniform
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-    // Projection matrix: 45 deg field of view, 4:3 ratio, 0.1<->100 unit
-    // distance
-    glm::mat4 Projection = glm::perspective(
-        glm::radians(45.0f), 4.0f/3.0f, 0.1f, 100.0f);
-
-    // Camera Matrix
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(4, 3, 3), // Camera is at (4, 3, 3) in world coors
-        glm::vec3(0, 0, 0), // Looks at the origin
-        glm::vec3(0, 1, 0)  // Head is up
-    );
+    GLuint Texture = loadDDS("uvtemplate.DDS");
+    // Get a handle for our "myTextureSampler" uniform
+    GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
 
     // Model matrix (identity): model will be at origin
     glm::mat4 Model = glm::mat4(1.0f);
@@ -255,9 +253,6 @@ int main( void )
         glm::mat4(1.0f), glm::vec3(2.6f, 2.9f, 2.0f)
     );
     TriangleModel = glm::translate(TriangleModel, glm::vec3(0.0f, 0.0f, 0.0f));
-    // Our Model View Projection, remember to use reverse order
-    glm::mat4 mvp = Projection * View * Model;
-    glm::mat4 mvptriangle = Projection * View * TriangleModel;
 
     do{
         // Clear the screen
@@ -265,14 +260,14 @@ int main( void )
 
         // Use our shader
         glUseProgram(programID);
+        computeMatricesFromInputs();
+        glm::mat4 View = getViewMatrix();
+        glm::mat4 Projection = getProjectionMatrix();
 
-        // Change the colors
-        for(int i = 0; i < 12*3*3; i++){
-            g_color_buffer_data[i] += 0.01f;
-            if(g_color_buffer_data[i] > 1.0f){
-                g_color_buffer_data[i] = 0.0f;
-            }
-        }
+        // Our Model View Projection, remember to use reverse order
+        glm::mat4 mvp = Projection * View * Model;
+        glm::mat4 mvptriangle = Projection * View * TriangleModel;
+
         glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
         glBufferData(
             GL_ARRAY_BUFFER,
@@ -284,6 +279,12 @@ int main( void )
         // Send transformation to the currently bound shader
         // Done in the loop because each model will have a different MVP matrix
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
+        // Bind our texture in Texture Unit 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, Texture);
+        // Set our "myTextureSampler" sampler to use Texture Unit 0
+        glUniform1i(TextureID, 0);
 
         // Draw cube
         // 1st attribute buffer: verticies
@@ -297,12 +298,12 @@ int main( void )
             0,          // stride
             (void*)0    // array buffer offset
         );
-        // 2nd attribute buffer: colors
+        // 2nd attribute buffer: texture
         glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
         glVertexAttribPointer(
             1,
-            3,
+            2,
             GL_FLOAT,
             GL_FALSE,
             0,
@@ -311,31 +312,11 @@ int main( void )
         // Actually draw cube
         // Starting from vertex 0; 12*3 verts total
         glDrawArrays(GL_TRIANGLES, 0, 12*3);
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
 
         // Draw triangle now
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvptriangle[0][0]);
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, trianglebuffer);
-        glVertexAttribPointer(
-            0,
-            3,
-            GL_FLOAT,
-            GL_FALSE,
-            0,
-            (void*)0
-        );
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-        glVertexAttribPointer(
-            1,
-            3,
-            GL_FLOAT,
-            GL_FALSE,
-            0,
-            (void*)0
-        );
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
@@ -348,71 +329,4 @@ int main( void )
             glfwWindowShouldClose(window) == 0);
 
 	return 0;
-}
-
-GLuint loadTGA_glfw(const char * imagepath){
-    // Create an OpenGL texture
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    // "Bind: the newly created texture
-    glBindTexture(GL_TEXTURE_2D, textureID);
-
-    // Read the file
-    glfwLoadTexture2D(imagepath, 0);
-
-    // trilinear filtering?
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glGenerateMipMap(GL_TEXTURE_2D);
-
-    return textureID;
-}
-
-GLuint loadBMP_custom(const char * imagepath){
-    unsigned char header[54];   // BMP headers are 54 bytes
-    unsigned int dataPos;       // Pos in file where data begins
-    unsigned int width, height;
-    unsigned int imageSize;     // width*height*3
-    unsigned char *data;        // Actual RGB data
-    // Open the file
-    FILE * file = fopen(imagepath, "rb");
-    if(!file){
-        printf("Image could not be opened\n");
-        return 0;
-    }
-    if(fread(header, 1, 54, file) != 54){
-        // If we can't read 54 bytes --> problem
-        printf("Not a correct BMP file\n");
-        return 0;
-    }
-    if(header[0] != 'B' || header[1] != 'M'){
-        // Header has to start with "BM"
-        printf("Not a correct BMP file\n");
-        return 0;
-    }
-    // read ints from the header
-    dataPos = *(int*)&(header[0x0A]);
-    imageSize = *(int*)&(header[0x22]);
-    width = *(int*)&(header[0x12]);
-    height = *(int*)&(header[0x16]);
-    // Guess info if missing
-    if(imageSize == 0){imageSize = width * height*3;}   // *3 for RGB
-    if(dataPos == 0){dataPos = 54;}
-    data = new unsigned char [imageSize];
-    fread(data, 1, imageSize, file);
-    fclose(file);
-    // Create an OpenGL texture
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    // "Bind" the newly created texture : all future texture functions affect
-    // this texture
-    glBindTexture(GL_TEXTURE_2D, textureID);
-
-    // Give image to OpenGL
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR,
-        GL_UNSIGNED BYTE, data);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 }
