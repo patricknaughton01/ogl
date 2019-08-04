c879b337b4411eb81f9cd2bed98a6d89f55396cb
Patrick Naughton <patricknaughton01@gmail.com>
2019-08-04 19:26:15 -0400

    tutorial07

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <vector>
 
 #include <GL/glew.h>
 
@@ -14,6 +15,7 @@ using namespace glm;
 #include <common/shader.hpp>
 #include <common/texture.hpp>
 #include <common/controls.hpp>
+#include <common/objloader.hpp>
 
 #define PI 3.141592653589793238f
 
@@ -36,7 +38,7 @@ int main( void )
 
     // Open a window and create its OpenGL context
     // GLFWwindow* window;
-    window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+    window = glfwCreateWindow(1024, 768, "Tutorial 07", NULL, NULL);
     if (window == NULL){
         fprintf(stderr, "Failed to open GLFW window. If you have an Intel"
                 "GPU, they are not 3.3 compatible. Try version 2.1\n");
@@ -61,62 +63,10 @@ int main( void )
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
 
-    // triangle verticies
-    static const GLfloat triangle_verticies[] = {
-        -1.0, -1.0, 0.0,
-        1.0, -1.0, 0.0,
-        0.0, 1.0, 0.0
-    };
-    GLuint trianglebuffer;
-    glGenBuffers(1, &trianglebuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, trianglebuffer);
-    glBufferData(
-        GL_ARRAY_BUFFER,
-        sizeof(triangle_verticies),
-        triangle_verticies,
-        GL_STATIC_DRAW
-    );
-
-    // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-    // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-    static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f,-1.0f,-1.0f, // triangle 1 : begin
-        -1.0f,-1.0f, 1.0f,
-        -1.0f, 1.0f, 1.0f, // triangle 1 : end
-        1.0f, 1.0f,-1.0f, // triangle 2 : begin
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f,-1.0f, // triangle 2 : end
-        1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        -1.0f,-1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f,-1.0f,
-        -1.0f, 1.0f,-1.0f,
-        1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f
-    };
+    std::vector<glm::vec3> verticies;
+    std::vector<glm::vec2> uv;
+    std::vector<glm::vec3> normals;
+    bool res = loadOBJ("cube.obj", verticies, uv, normals);
 
     // Used to id our buffer
     GLuint vertexbuffer;
@@ -126,106 +76,18 @@ int main( void )
     // Put our actual verticies into the buffer
     glBufferData(
         GL_ARRAY_BUFFER,
-        sizeof(g_vertex_buffer_data),
-        g_vertex_buffer_data,
+        verticies.size() * sizeof(glm::vec3),
+        &verticies[0],
         GL_STATIC_DRAW
     );
 
-    // One color for each vertex. They were generated randomly.
-    static GLfloat g_color_buffer_data[] = {
-        0.583f,  0.771f,  0.014f,
-        0.609f,  0.115f,  0.436f,
-        0.327f,  0.483f,  0.844f,
-        0.822f,  0.569f,  0.201f,
-        0.435f,  0.602f,  0.223f,
-        0.310f,  0.747f,  0.185f,
-        0.597f,  0.770f,  0.761f,
-        0.559f,  0.436f,  0.730f,
-        0.359f,  0.583f,  0.152f,
-        0.483f,  0.596f,  0.789f,
-        0.559f,  0.861f,  0.639f,
-        0.195f,  0.548f,  0.859f,
-        0.014f,  0.184f,  0.576f,
-        0.771f,  0.328f,  0.970f,
-        0.406f,  0.615f,  0.116f,
-        0.676f,  0.977f,  0.133f,
-        0.971f,  0.572f,  0.833f,
-        0.140f,  0.616f,  0.489f,
-        0.997f,  0.513f,  0.064f,
-        0.945f,  0.719f,  0.592f,
-        0.543f,  0.021f,  0.978f,
-        0.279f,  0.317f,  0.505f,
-        0.167f,  0.620f,  0.077f,
-        0.347f,  0.857f,  0.137f,
-        0.055f,  0.953f,  0.042f,
-        0.714f,  0.505f,  0.345f,
-        0.783f,  0.290f,  0.734f,
-        0.722f,  0.645f,  0.174f,
-        0.302f,  0.455f,  0.848f,
-        0.225f,  0.587f,  0.040f,
-        0.517f,  0.713f,  0.338f,
-        0.053f,  0.959f,  0.120f,
-        0.393f,  0.621f,  0.362f,
-        0.673f,  0.211f,  0.457f,
-        0.820f,  0.883f,  0.371f,
-        0.982f,  0.099f,  0.879f
-    };
-    GLuint colorbuffer;
-    glGenBuffers(1, &colorbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-    glBufferData(
-        GL_ARRAY_BUFFER,
-        sizeof(g_color_buffer_data),
-        g_color_buffer_data,
-        GL_STREAM_DRAW
-    );
-
-    // Two UV coordinatesfor each vertex. They were created with Blender. You'll learn shortly how to do this yourself.
-    static const GLfloat g_uv_buffer_data[] = {
-        0.000059f, 1.0f-0.000004f,
-        0.000103f, 1.0f-0.336048f,
-        0.335973f, 1.0f-0.335903f,
-        1.000023f, 1.0f-0.000013f,
-        0.667979f, 1.0f-0.335851f,
-        0.999958f, 1.0f-0.336064f,
-        0.667979f, 1.0f-0.335851f,
-        0.336024f, 1.0f-0.671877f,
-        0.667969f, 1.0f-0.671889f,
-        1.000023f, 1.0f-0.000013f,
-        0.668104f, 1.0f-0.000013f,
-        0.667979f, 1.0f-0.335851f,
-        0.000059f, 1.0f-0.000004f,
-        0.335973f, 1.0f-0.335903f,
-        0.336098f, 1.0f-0.000071f,
-        0.667979f, 1.0f-0.335851f,
-        0.335973f, 1.0f-0.335903f,
-        0.336024f, 1.0f-0.671877f,
-        1.000004f, 1.0f-0.671847f,
-        0.999958f, 1.0f-0.336064f,
-        0.667979f, 1.0f-0.335851f,
-        0.668104f, 1.0f-0.000013f,
-        0.335973f, 1.0f-0.335903f,
-        0.667979f, 1.0f-0.335851f,
-        0.335973f, 1.0f-0.335903f,
-        0.668104f, 1.0f-0.000013f,
-        0.336098f, 1.0f-0.000071f,
-        0.000103f, 1.0f-0.336048f,
-        0.000004f, 1.0f-0.671870f,
-        0.336024f, 1.0f-0.671877f,
-        0.000103f, 1.0f-0.336048f,
-        0.336024f, 1.0f-0.671877f,
-        0.335973f, 1.0f-0.335903f,
-        0.667969f, 1.0f-0.671889f,
-        1.000004f, 1.0f-0.671847f,
-        0.667979f, 1.0f-0.335851f
-    };
     GLuint uvbuffer;
     glGenBuffers(1, &uvbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
     glBufferData(
         GL_ARRAY_BUFFER,
-        sizeof(g_uv_buffer_data),
-        g_uv_buffer_data,
+        uv.size() * sizeof(glm::vec2),
+        &uv[0],
         GL_STATIC_DRAW
     );
 
@@ -244,16 +106,12 @@ int main( void )
     // Get a handle on our "MVP" uniform
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-    GLuint Texture = loadDDS("uvtemplate.DDS");
+    GLuint Texture = loadDDS("uvmap.DDS");
     // Get a handle for our "myTextureSampler" uniform
     GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
 
     // Model matrix (identity): model will be at origin
     glm::mat4 Model = glm::mat4(1.0f);
-    glm::mat4 TriangleModel = glm::scale(
-        glm::mat4(1.0f), glm::vec3(2.6f, 2.9f, 2.0f)
-    );
-    TriangleModel = glm::translate(TriangleModel, glm::vec3(0.0f, 0.0f, 0.0f));
     long frame = 0;
     float cam_speed = 0.01f;
     float rad = 6.0f;
@@ -266,7 +124,7 @@ int main( void )
 
         // Use our shader
         glUseProgram(programID);
-        glm::vec3 position = glm::vec3(rad * cos(frame * cam_speed), 4.0f,
+        glm::vec3 position = glm::vec3(rad * cos(frame * cam_speed), -4.0f,
             -rad * sin(frame * cam_speed));
         glm::vec3 right = glm::vec3(
             -rad * sin(frame * cam_speed), 0.0f, -rad * cos(frame * cam_speed)
@@ -280,15 +138,6 @@ int main( void )
 
         // Our Model View Projection, remember to use reverse order
         glm::mat4 mvp = Projection * View * Model;
-        glm::mat4 mvptriangle = Projection * View * TriangleModel;
-
-        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-        glBufferData(
-            GL_ARRAY_BUFFER,
-            sizeof(g_color_buffer_data),
-            g_color_buffer_data,
-            GL_STATIC_DRAW
-        );
 
         // Send transformation to the currently bound shader
         // Done in the loop because each model will have a different MVP matrix
@@ -325,13 +174,8 @@ int main( void )
         );
         // Actually draw cube
         // Starting from vertex 0; 12*3 verts total
-        glDrawArrays(GL_TRIANGLES, 0, 12*3);
+        glDrawArrays(GL_TRIANGLES, 0, verticies.size());
 
-        // Draw triangle now
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvptriangle[0][0]);
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, trianglebuffer);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
 
